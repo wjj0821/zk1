70743b0a9f808988661ce78cd7b8434b775e47fb
wjj <1649507554@qq.com>
2018-09-03 09:33:14 +0800

    框架

---
+++ gulpfile.js
@@ -0,0 +1,46 @@
+var gulp = require("gulp");
+var fs = require("fs");
+var path = require("path");
+var url = require("url");
+var scss = require("gulp-sass"); //scss转为css
+var mincss = require("gulp-clean-css"); //压缩css
+var minJs = require("gulp-uglify");
+var server = require("gulp-webserver");
+
+//转为css并压缩
+gulp.task("sassTask", function() {
+        return gulp.src("./src/scss/*.scss")
+            .pipe(scss())
+            .pipe(mincss())
+            .pipe(gulp.dest("./src/css"))
+    })
+    //监听scss的变化
+gulp.task("watch", function() {
+        return gulp.src("./src/scss/index.scss", gulp.series("sassTask"));
+    })
+    //压缩js
+gulp.task("minjs", function() {
+        return gulp.src(["./src/js/**/*.js", "!./src/js/lib/*.js"])
+            .pipe(minJs())
+            .pipe(gulp.dest("./src/jj"))
+    })
+    //起服务 判断接口
+gulp.task("server", function() {
+    return gulp.src("src")
+        .pipe(server({
+            port: 8998,
+            middleware: function(req, res, next) {
+                var pathname = url.parse(req.url).pathname;
+                if (pathname === "/") {
+                    return false;
+                }
+                if (pathname === "/api/list") {
+
+                } else if (pathname === "/") {
+                    var pathname = pathname === "/" ? "/index.html" : pathname;
+                    res.end(fs.readFileSync(path.join(__dirname, "src", pathname)));
+                }
+            }
+        }))
+})
+gulp.task("dev", gulp.series("sassTask", "minjs", "server", "watch"));
